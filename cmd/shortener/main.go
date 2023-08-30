ec5bbf2413ba96abb06cb07fe2b5fa9266df6d1f
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-08-30 17:58:49 +0300

    chi router + mw

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,22 +3,30 @@ package main
 import (
 	"os"
 	"shortener/config"
+	mwLogger "shortener/http-server/middleware/logger"
 	"shortener/lib/logger/sl"
 	"shortener/storage/mysql"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"golang.org/x/exp/slog"
 )
 
 func main() {
 	cfg := config.MustLoad()
+
 	log := setupLogger()
-	log.Info("starting app", slog.String("env", cfg.Env))
+
 	storage, err := mysql.New(cfg.StoragePath)
 	if err != nil {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
 	log.Info("Stogage init", storage)
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(mwLogger.New(log))
 }
 
 func setupLogger() *slog.Logger {
